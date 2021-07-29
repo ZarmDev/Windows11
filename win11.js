9b39ddc1ac7c87add21e463af91426ce06f1d620
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2021-07-29 14:01:20 -0400

    Out start menu

--- win11.js
+++ win11.js
@@ -4,6 +4,8 @@ const windows = document.getElementById('windows');
 
 const batteryLevel = document.getElementById('battery-level');
 
+const wallpaper = document.getElementById('wallpaper');
+
 window.toggled = false;
 
 windows.addEventListener('mousedown', function () {
@@ -35,6 +37,32 @@ windows.addEventListener('mousedown', function () {
             t += 0.1;
             if (i > 80) {
                 clearInterval(myInterval)
+            }
+        }, 1)
+    }
+})
+
+window.check = false;
+
+wallpaper.addEventListener('mousedown', function (e) {
+    window.check = true;
+})
+
+document.getElementsByTagName('html')[0].addEventListener('mousedown', function () {
+    if (window.check == true) {
+        window.check = false;
+        window.toggled = false;
+        var i = 100;
+        var t = 1;
+        var myInterval2 = setInterval(function () {
+            windowsClick.style.bottom = `${i}px`;
+            windowsClick.style.opacity = `${t}`;
+            console.log(i, t);
+            i -= 6;
+            t -= 0.05;
+            if (i < 20) {
+                clearInterval(myInterval2)
+                windowsClick.style.visibility = 'hidden';
             }
         }, 1)
     }
